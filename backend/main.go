b37a26866a9a999c56ac4939146a0936753564e9
Carlos Amaral <carlos.a.amaral@sesisc.org.br>
2022-07-11 00:32:08 -0300

    Adding api

---
+++ backend/main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// MODELS
+type ClientModel struct {
+	ID           string
+	FirstName    string
+	LastName     string
+	FullName     string
+	Birthdate    string
+	StartDate    string
+	LastVisit    string
+	Cpf          string
+	Payment      PaymentModel
+	Train        TrainModel
+	TrainHistory []TrainModel
+}
+
+type InstructorModel struct {
+	ID        string
+	FirstName string
+	FullName  string
+	LastName  string
+	Birthdate string
+	StartDate string
+	Cpf       string
+	Salary    float64
+	Paid      bool
+}
+
+type TrainModel struct {
+	ID        string
+	Name      string
+	Duration  float64
+	CreatedBy InstructorModel
+	Exercises []ExerciseModel
+}
+
+type ExerciseModel struct {
+	ID                string
+	Title             string
+	Duration          float64
+	Repetition        int
+	Sets              int
+	RecommendedWeight int
+	MachineNumber     int
+}
+
+type PaymentModel struct {
+	ID    string
+	value string
+}
+
+var ClientData = []ClientModel{
+	{
+		ID:        "0",
+		FirstName: "Carlos",
+		LastName:  "Amaral",
+		FullName:  "Carlos Alberto Barcelos do Amaral",
+		Birthdate: "00/00/0000",
+		StartDate: "03/02/2021",
+		LastVisit: "11/07/2022",
+		Cpf:       "000.000.000-00",
+		Payment: PaymentModel{
+			ID:    "0",
+			value: "200",
+		},
+		Train: TrainData[0],
+	},
+
+	{
+		ID:        "1",
+		FirstName: "Ana",
+		LastName:  "Exemplo",
+		FullName:  "Ana Clara Exemplo",
+		Birthdate: "00/00/0000",
+		StartDate: "24/01/2022",
+		LastVisit: "10/04/2022",
+		Cpf:       "000.000.000-00",
+		Payment: PaymentModel{
+			ID:    "0",
+			value: "200",
+		},
+		Train: TrainData[1],
+	},
+}
+
+var InstructorData = []InstructorModel{
+	{"0", "João", "João Treinador Exemplo", "Exemplo", "00/00/0000", "00/00/0000", "000.000.000-00", 2500.90, false},
+	{"1", "Fulano", "Fulano Treinador Exemplo", "Exemplo", "00/00/0000", "00/00/0000", "000.000.000-00", 3500.90, false},
+}
+
+var TrainData = []TrainModel{
+	{
+		ID:        "0",
+		Name:      "Treino | Carlos Amaral",
+		Duration:  49.5,
+		CreatedBy: InstructorData[0],
+		Exercises: []ExerciseModel{
+			ExerciseData[0],
+			ExerciseData[1],
+		},
+	},
+
+	{
+		ID:        "1",
+		Name:      "Treino | Ana Exemplo",
+		Duration:  30,
+		CreatedBy: InstructorData[1],
+		Exercises: []ExerciseModel{
+			ExerciseData[0],
+		},
+	},
+}
+
+var ExerciseData = []ExerciseModel{
+	{"0", "Flexão no solo", 10.0, 10, 3, 0, 0},
+	{"1", "Cadeira Extensora", 5.0, 13, 3, 30, 8},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/client/all", getAllClients)
+	router.GET("/client/:id", getClientByID)
+	router.POST("/client/new", newClient)
+	router.DELETE("/client/delete/:id", deleteClient)
+
+	router.GET("/instructor/all", getAllInstructors)
+	router.GET("/instructor/:id", instructorById)
+	//router.SetTrustedProxies([]string{"127.0.0.1"})
+	err := router.Run()
+	if err != nil {
+		return
+	}
+}
+
+// MARK: CONTROLLERS
+func getAllClients(c *gin.Context) { c.IndentedJSON(http.StatusOK, ClientData) }
+func getClientByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, client := range ClientData {
+		if client.ID == id {
+			c.IndentedJSON(http.StatusOK, client)
+		}
+	}
+}
+
+func newClient(c *gin.Context) {
+	var client ClientModel
+	if err := c.Bind(&client); err != nil {
+		return
+	}
+
+	ClientData = append(ClientData, client)
+	c.IndentedJSON(http.StatusCreated, client)
+}
+
+func deleteClient(c *gin.Context) {
+	id := c.Param("id")
+
+	var newList []ClientModel
+
+	for _, u := range ClientData {
+		if u.ID != id {
+			newList = append(newList, u)
+		}
+	}
+	ClientData = newList
+
+	c.IndentedJSON(http.StatusAccepted, ClientData)
+}
+
+func getAllInstructors(c *gin.Context) { c.IndentedJSON(http.StatusOK, InstructorData) }
+func instructorById(c *gin.Context) {
+	id := c.Param("id")
+	var obj InstructorModel
+
+	for _, o := range InstructorData {
+		if o.ID == id {
+			obj = o
+		}
+	}
+
+	if obj.ID == "" {
+		c.IndentedJSON(http.StatusNotFound, nil)
+	} else {
+		c.IndentedJSON(http.StatusFound, obj)
+	}
+}
